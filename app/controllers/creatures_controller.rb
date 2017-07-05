c0ac403dd5853a93532375dc8c9df8986ea87460
katemontha <katemontha@gmail.com>
2017-07-05 14:35:51 -0400

    full crud app

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,4 +1,5 @@
 class CreaturesController < ApplicationController
+
   def index
     @creatures = Creature.all
   end
@@ -8,7 +9,6 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
     if creature.save
       redirect_to creature_path(creature)
@@ -18,4 +18,26 @@ class CreaturesController < ApplicationController
   def show
     @creature = Creature.find(params[:id])
   end
+
+  def edit
+    @creature = Creature.find(params[:id])
+  end
+
+  def update
+    creature = Creature.find_by_id(params[:id])
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+  def destroy
+    creature = Creature.find_by_id(params[:id])
+    creature.destroy
+    redirect_to creatures_path
+  end
+
+  private
+  def creature_params
+    params.require(:creature)
+          .permit(:name, :description)
+    end
 end
